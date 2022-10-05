6ccbdf6df864bd67f2f8846aa1bd307a91b572e2
Aylen <ayleeenmaliandi@gmail.com>
2022-10-05 18:57:27 -0300

    agrego sweetalert y otra funcionalidad a la página

---
+++ servicio.js
@@ -0,0 +1,21 @@
+let boton1 = document.getElementById("btnPrimero")
+      boton1.onclick = () =>{console.log(swal("Tu dieta elegida fue Vegana", "Se realizó la contratación del sevicio exitosamente","success"))}
+      let boton2 = document.getElementById("btnSegundo")
+      boton2.onclick = () =>{console.log(swal("Tu dieta elegida fue Vegetariana", "Se realizó la contratación del sevicio exitosamente","success"))}
+      let boton3 = document.getElementById("btnTercero")
+      boton3.onclick = () =>{console.log(swal("Tu dieta elegida fue Omnívera", "Se realizó la contratación del sevicio exitosamente","success"))}
+// Elegí sweetalaert para que al apretar el botón de contratar servicio, este confirmo que se realizó la contratación
+      let container = document.getElementById("intro")
+container.innerHTML = "<h2>Planes Alimenticios </h2><p>VOBIS SANUS aparece debido a que es un emprendimiento único y original en su área</p> <p>Buscamos adaptarnos a cualquier tipo de dieta alimenticia, también lograr que las personas se alimenten de una mejor manera. Y finalmente evitar la estresante pregunta; ¿Qué comemos hoy?</p><p>Nos dedicamos a la venta de viandas y productos saludables en conjunto. Queremos que la gente se alimente de la mejor forma sin tener que moverse de su casa y perder tiempo en las larguísimas colas de los supermercados. </p><p>Nuestros productos son 100% naturales y orgánicos. Cada elaboración nuestra, trae consigo la lista de productos que se utilizó para crearlo. SIEMPRE INVITAMOS A QUE LA GENTE CONOZCA LO QUE CONSUME !!</p><p>Nuestro servicio no genera aburrimiento o cansancio. La adquisición del servicio incluye un libro con menús para cada tipo de dieta. Evitando así que se queden sin ideas sobre ¿Qué comer?.Además las viandas que se incluyan en la prestación, todas las semanas van a ir variando</p><p>Las tarjetas informativas van a comunicarles todo lo que incluye el servicio. Las viandas y los alimentos, se entregarán en la puerta de los clientes cada dos semanas. Ante cualquier duda, no dudes en contactarnos por nuestras redes!</p>"  
+    
+
+    
+   
+    
+   
+    
+   
+    
+
+  
+
